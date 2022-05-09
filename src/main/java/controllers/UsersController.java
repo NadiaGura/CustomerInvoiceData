d8015393d689178f0e4415ff050b5b7dc3d5ee9e
NadiaGura <gura.nadezda@gmail.com>
2022-05-09 12:56:01 +0300

    added methods for salesManager(read inventory and customers)

---
+++ src/main/java/controllers/UsersController.java
@@ -0,0 +1,99 @@
+package controllers;
+
+import objects.Customer;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class UsersController {
+
+
+    public class StudentController {
+
+        //initialize scanner
+        private static Scanner sc = new Scanner(System.in);
+        private static PreparedStatement ps;
+        private static ResultSet rs;
+
+        //METHOD TO GET/READ INVENTORY BY ID/NAME
+        public static boolean getProductById(){
+            //ask user for data
+            System.out.println("Please enter product ID: ");
+            int id = sc.nextInt();
+
+//            System.out.println("Enter name of the product: ");
+//            String name = sc.next();
+
+
+            //UPDATE WITH CORRECT QUERY FROM DB//
+            try {
+                ps = getConnection().prepareStatement("SELECT * FROM inventory WHERE id = " + id);
+                rs = ps.executeQuery();
+
+                //define var to temporarily hold each field in the result set.
+                int id, price;
+                String name;
+
+                //instantiate the student objet to return at the end of the method execution
+                Inventory inventory = new Inventory();
+
+                //loop through the result set and add the necessary values in the inventory object
+                while (rs.next()) {
+                    id = rs.getInt("id");
+                    name = rs.getString("name");
+                    price = rs.getInt("price");
+                    inventory.setName(name);
+                    inventory.setId(id);
+                    inventory.setPrice(price);
+                }
+                return inventory;
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+
+
+        //METHOD TO GET/READ CUSTOMERS BY ID/NAME
+
+        public static boolean getCustomerById(){
+            //ask user for data
+            System.out.println("Please enter customer ID: ");
+            int id = sc.nextInt();
+
+//            System.out.println("Enter name of the customer: ");
+//            String name = sc.next();
+
+
+            //UPDATE WITH CORRECT QUERY FROM DB//
+            try {
+                ps = getConnection().prepareStatement("SELECT * FROM customers WHERE id = " + id);
+                rs = ps.executeQuery();
+
+                //define var to temporarily hold each field in the result set.
+                int id;
+                String name;
+
+                //instantiate the student objet to return at the end of the method execution
+                Customer customer = new Customer();
+
+                //loop through the result set and add the necessary values in customer object
+                while (rs.next()) {
+                    id = rs.getInt("id");
+                    name = rs.getString("name");
+                    customer.setName(name);
+                    customer.setId(id);
+                }
+                return customer;
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
+}
--- src/main/java/objects/Users.java
+++ src/main/java/objects/Users.java
@@ -4,9 +4,35 @@ public class Users {
 
     private int id;
     private String name;
-    private String role;
+    private String role; //admin/ salesManager
 
     //getters setters
+    public int getId() {
+        return id;
+    }
 
-    //constructor
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    //empty constructor
+    public Users() {
+    }
+
 }
