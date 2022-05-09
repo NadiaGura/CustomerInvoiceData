6baea06641cce12e812a6b303280d4b8c6b8be6f
NadiaGura <gura.nadezda@gmail.com>
2022-05-09 20:16:52 +0300

    DB Connection

--- src/main/java/dbhelper/DBConnection.java
+++ src/main/java/dbhelper/DBConnection.java
@@ -1,4 +1,18 @@
 package dbhelper;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DBConnection {
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/inventory", "postgres", "");
+            if (connection != null) System.out.println("Connection successful!");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("There was a problem connection to the DB.");
+        }
+        return connection;
+    }
 }
