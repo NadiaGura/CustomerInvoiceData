d762e4ed7878d21d38fe30087cc2c1f597128264
shooshanaa <zane@kruming.lv>
2022-05-13 19:50:37 +0300

    invoice changes

--- src/main/java/company/controllers/InvoiceController.java
+++ src/main/java/company/controllers/InvoiceController.java
@@ -5,6 +5,9 @@ import company.objects.Invoice;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
 
 import static company.dbhelper.DBConnection.getConnection;
@@ -15,13 +18,51 @@ public class InvoiceController {
     private static PreparedStatement ps;
     private static ResultSet rs;
 
-    public static boolean createInvoice(){
+
+       /* public List<Invoice> all(){
+            List<Invoice> allInvoices = new ArrayList<>();
+
+            try {
+                ps=getConnection().prepareStatement("SELECT * FROM invoices");
+                rs = ps.executeQuery();
+
+                while (rs.next()){
+                    String name = rs.getString("name");
+                    int id = rs.getInt("id");
+                    Date date= rs.getDate("date");
+                    allInvoices.add(new Invoice(id, name, date));
+
+                }
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }finally {
+                return allInvoices;
+            }
+
+
+        }*/
+
+        public static boolean createInvoice () {
+        /*    Invoice inv = new Invoice();
+            try {
+                ps = getConnection().prepareStatement("INSERT INTO invoices (id, name, date) values (?, ?, ?");
+                ps.setInt(1, inv.getId());
+                ps.setString(2, inv.getCustomer());
+                ps.setDate(3, inv.getDate());
+                ps.execute();
+
+            } catch (SQLException e) {
+              //  throw new RuntimeException(e);
+            }
+        }*/
+
+
         System.out.print("Enter the customers id: ");
         int customersid = scanner.nextInt();
 
         System.out.print("Enter the date of the invoice: ");
         String invoiceDate = scanner.next();
-
 
         try {
             ps = getConnection().prepareStatement("INSERT INTO invoices (customersid, date) VALUES(?, ?)");
@@ -60,14 +101,14 @@ public class InvoiceController {
         System.out.println("Enter the id of the invoice: ");
         int id = scanner.nextInt();
 
-        System.out.println("Enter new customersid:");
-        int newCustomersId = scanner.nextInt();
+     //   System.out.println("Enter new customersid:");
+     //     int newCustomersId = scanner.nextInt();
 
         System.out.println("Enter new date:");
         String newDate = scanner.next();
 
         try {
-            ps = getConnection().prepareStatement("UPDATE invoices SET customersid ='" + newCustomersId + "' WHERE id =" + id);
+         //   ps = getConnection().prepareStatement("UPDATE invoices SET customersid ='" + newCustomersId + "' WHERE id =" + id);
             ps = getConnection().prepareStatement("UPDATE products SET price ='" + newDate + "' WHERE id =" + id);
             ps.execute();
             return true;
--- src/main/java/company/objects/Invoice.java
+++ src/main/java/company/objects/Invoice.java
@@ -1,9 +1,44 @@
 package company.objects;
 
 import java.util.Date;
+import java.util.List;
 
 public class Invoice {
-  //  Date date = new Date();
+
+  /*  private int id;
+    private Customer customer;
+    private Date date;
+
+    public Invoice() {
+
+    }
+
+    public Invoice(int id, Customer customer, Date date) {
+        this.id = id;
+        this.customer = customer;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public java.sql.Date getDate() {
+        return (java.sql.Date) date;
+    }
+
+
+
+
+
+
+
+}*/
+
 
     private int id; // get all invoice data (items list and total amount)
     private int customersid;
@@ -15,6 +50,10 @@ public class Invoice {
         this.id = id;
         this.customersid = customersid;
         this.invoiceDate = invoiceDate;
+    }
+
+    public Invoice() {
+
     }
 
 
@@ -41,4 +80,51 @@ public class Invoice {
     public void setInvoiceDate(String invoiceDate) {
         this.invoiceDate = invoiceDate;
     }
+
+  /*  private int id;
+    private static Customer customer = new Customer();
+    private static Date invoiceDate;
+    private static List<InvoiceLine> invoiceLines;
+
+    public Invoice(int id, Customer customer, Date invoiceDate, List<InvoiceLine>invoiceLines) {
+        this.id = id;
+        this.customer = customer;
+        this.invoiceDate = invoiceDate;
+        this.invoiceLines = invoiceLines;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+
+    public String getCustomerName(){
+        return customer.getName();
+    }
+
+    public int getCustomerId(){
+        return customer.getId();
+    }
+
+
+    public Date getInvoiceDate() {
+        return invoiceDate;
+    }
+
+    public void setInvoiceDate(Date invoiceDate) {
+        this.invoiceDate = invoiceDate;
+    }
+
+    public List<InvoiceLine> getInvoiceLines() {
+        return invoiceLines;
+    }*/
 }
