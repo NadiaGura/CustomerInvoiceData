df39d2429e24b428ba27415a9e6eac187ca21e2d
shooshanaa <zane@kruming.lv>
2022-05-13 19:17:47 +0300

    invoice line commented out with some new ideas

--- src/main/java/company/objects/InvoiceLine.java
+++ src/main/java/company/objects/InvoiceLine.java
@@ -1,40 +1,63 @@
 package company.objects;
 
-public class InvoiceLine {
-    private int id;
-    private int invoiceid;
-    private int productid;
+public class InvoiceLine  {
+    /*private int id;
     private int quantity;
-    private float sum;
-    private float totalprice;
+    private Product product;
+    private Invoice invoice;
+    private int productid;
+    private int invoiceid;
+
+    //   private int invoiceid;
+    //   private int productid;
+    //   private float price;
+    //   private float sum;
+    //   private float totalprice;
 
     public InvoiceLine() {
 
+
     }
+
+    public InvoiceLine(int id, int quantity, int productid, int invoiceid) {
+        this.id = id;
+        this.quantity = quantity;
+        this.productid = productid;
+        this.invoiceid = invoiceid;
+    }
+
 
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public int getInvoiceId(){
+        return getInvoiceId();
     }
 
-    public int getInvoiceid() {
+    public int getProductId(){
+        return getProductId();
+    }
+
+ *//*   public void setId(int id) {
+        this.id = id;
+    }*//*
+
+ *//*   public int getInvoiceid() {
         return invoiceid;
     }
 
     public void setInvoiceid(int invoiceid) {
         this.invoiceid = invoiceid;
     }
-
-    public int getProductid() {
+    *//*
+ *//*   public int getProductid() {
         return productid;
     }
 
     public void setProductid(int productid) {
         this.productid = productid;
-    }
+    }*//*
 
     public int getQuantity() {
         return quantity;
@@ -44,7 +67,7 @@ public class InvoiceLine {
         this.quantity = quantity;
     }
 
-    public float getSum() {
+   *//* public float getSum() {
         return sum;
     }
 
@@ -59,5 +82,26 @@ public class InvoiceLine {
     public void setTotalprice(float totalprice) {
         this.totalprice = totalprice;
     }
-}
+*//*
+     public String getProductName(){
+       return getProductName();
+    }
 
+    public float getProductPrice(){
+         return getProductPrice();
+    }
+
+     public float getSum(){
+        return product.getPrice()*quantity;
+
+    }
+
+
+
+    public String toString(){
+        String invoiceLine = " ";
+        invoiceLine = (getProductName() + quantity + " total cost is " + getSum());
+        return invoiceLine;
+    }
+}
+*/
