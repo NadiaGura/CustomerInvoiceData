025dda992553c9c5217570128c93f4fe01cfb919
NadiaGura <gura.nadezda@gmail.com>
2022-05-13 10:38:33 +0300

    Forms

---
+++ src/main/java/company/Forms/WelcomeScreen.java
@@ -0,0 +1,16 @@
+package company.Forms;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class WelcomeScreen extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
