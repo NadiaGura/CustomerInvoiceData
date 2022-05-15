54d850c395e80e94c8d3122afd28a0ef3b0cdf95
NadezdaIS <nadezhdaivanova29@gmail.com>
2022-05-15 15:39:21 +0300

    Menu

---
+++ src/main/java/company/menu/WelcomeMenu.java
@@ -0,0 +1,29 @@
+package company.menu;
+
+import company.login.Authorisation;
+
+import java.util.Scanner;
+
+public class WelcomeMenu {
+    public static void menu() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome! Choose one: ");
+        System.out.println("1. Log in.");
+        System.out.println("2. Sign up.");
+
+        int option = scanner.nextInt();
+
+        switch (option) {
+            case 1:
+                System.out.println(Authorisation.login());
+                break;
+            case 2:
+                System.out.println(Authorisation.signUp());
+                break;
+
+            default:
+                System.out.println("Invalid option. Try again.");
+                menu();
+        }
+    }
+}
