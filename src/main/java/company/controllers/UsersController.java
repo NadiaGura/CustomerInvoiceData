e1b2636165715f49777eb383467a0894abe60094
NadiaGura <gura.nadezda@gmail.com>
2022-05-15 22:06:24 +0300

    deleting unnecessary controller from UsersController

--- src/main/java/company/controllers/UsersController.java
+++ src/main/java/company/controllers/UsersController.java
@@ -14,56 +14,6 @@ public class UsersController {
     private static PreparedStatement ps;
     private static ResultSet rs;
 
-
-    //   public class StudentController {
-
-//        //initialize scanner
-//        private Scanner sc = new Scanner(System.in);
-//        private PreparedStatement ps;
-//        private ResultSet rs;
-//
-//        //METHOD TO GET/READ INVENTORY BY ID/NAME
-//        public  boolean getProductById(){
-//            //ask user for data
-//            System.out.println("Please enter product ID: ");
-//            int id = sc.nextInt();
-//
-////            System.out.println("Enter name of the product: ");
-////            String name = sc.next();
-//
-//
-//            //UPDATE WITH CORRECT QUERY FROM DB//
-//            try {
-//                ps = getConnection().prepareStatement("SELECT * FROM inventory WHERE id = " + id);
-//                rs = ps.executeQuery();
-//
-//                //define var to temporarily hold each field in the result set.
-//                int id, price;
-//                String name;
-//
-//                //instantiate the student objet to return at the end of the method execution
-//                Inventory inventory = new Inventory();
-//
-//                //loop through the result set and add the necessary values in the inventory object
-//                while (rs.next()) {
-//                    id = rs.getInt("id");
-//                    name = rs.getString("name");
-//                    price = rs.getInt("price");
-//                    inventory.setName(name);
-//                    inventory.setId(id);
-//                    inventory.setPrice(price);
-//                }
-//                return inventory;
-//
-//            } catch (SQLException e) {
-//                e.printStackTrace();
-//                return null;
-//            }
-//        }
-//
-//
-//
-
     //ADD USER METHOD
     public static boolean addUser() {
         System.out.println("Enter username:");
